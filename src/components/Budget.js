bf404099b19fdd18495be41b24715651e0ec25e7
cagri32 <cagriahmet32@gmail.com>
2024-01-12 19:07:32 -0500

    Budget comp improvements

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,15 +1,27 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        if (event.target.value > 20000) {
+            alert("The value cannot exceed remaining funds  £20,000");
+        } else {
+            setNewBudget(event.target.value);
+        }
     }
+    useEffect(() => {
+        dispatch(newBudget);
+        if(newBudget > 20000 ){
+            alert("The value cannot exceed remaining funds  £20,000");
+            setNewBudget(20000)
+        }
+    }, [newBudget])
+
     return (
         <div className='alert alert-secondary'>
-            { <span>Budget: £</span> /*{budget} */}
+            {<span>Budget: £</span> /*{budget} */}
             <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
         </div>
     );
